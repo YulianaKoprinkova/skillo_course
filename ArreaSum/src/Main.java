504cac95ea877f45e4805786c8d7e0c7487d73e9
YulianaKoprinkova <yuliana.koprinkova@gmail.com>
2025-03-25 18:14:19 +0200

    removed space

---
+++ ArreaSum/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbersArray = {0, -15, 12, 15};
+        int sumArray = 0;
+
+        for (int number : numbersArray) sumArray += number;
+
+        System.out.println("The sum is: " + sumArray);
+    }
+
+}
+
+
