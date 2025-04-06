b7e98cc14bfee063d7e16701277cae876c26053c
YulianaKoprinkova <yuliana.koprinkova@gmail.com>
2025-04-06 20:45:53 +0300

    б

---
+++ averageOfThree/src/Main.java
@@ -0,0 +1,12 @@
+
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        public static int sumThreeIntNums(int num1, int num2, int num3) {
+            int sumOfNums = num1 + num2 + num3;
+            System.out.println("The \"sum\" is: " + sumOfNums);
+            return sumOfNums;
+        }
+
+
+    }
